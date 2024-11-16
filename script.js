c1774a87dbe91573d918afbfb4c23de538006394
Rishu00001 <ritiksingh7369@gmail.com>
2024-11-16 22:30:41 +0530

    second commit

--- script.js
+++ script.js
@@ -7,26 +7,31 @@ form.addEventListener("submit", (e) => {
 });
 
 const getWordInfo = async (word) => {
-  const response = await fetch(
-    `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
-  );
-  const data = await response.json();
-  let definitions = data[0].meanings[0].definitions[0];
-  result.innerHTML = `
-    <h2><strong>Word:</strong> ${data[0].word}</h2>
-    <p>${data[0].meanings[0].partOfSpeech}</p>
-      <p><strong>Meaning:</strong> ${definitions.definition || "Not Found"}</p>
-      <p><strong>Example:</strong> ${definitions.example || "Not Found"}</p>
-       <p><strong>Antonyms:</strong>
-    `;
-    if(definitions.antonyms.length === 0){
-        result.innerHTML += "Not Found"
-    }
-    else{
-        for(let i = 0; i< definitions.antonyms.length; i++){
-            result.innerHTML += `<li>${definitions.antonyms[i]}</li>`
-        }
-    }
-  result.innerHTML += `<a href = "${data[0].sourceUrls}" target = "_blank">Read more..</a>`
-  console.log(data);
+  try{
+    result.style.display = "block"
+    result.innerHTML = "Fetching..."
+    const response = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    );
+    const data = await response.json();
+    let definitions = data[0].meanings[0].definitions[0];
+    result.innerHTML = `
+      <h2><strong>Word:</strong> ${data[0].word}</h2>
+      <p class = "partOfSpeech">${data[0].meanings[0].partOfSpeech}</p>
+        <p><strong>Meaning:</strong> ${definitions.definition || "Not Found"}</p>
+        <p><strong>Example:</strong> ${definitions.example || "Not Found"}</p>
+         <p><strong>Antonyms:</strong>
+      `;
+      if(definitions.antonyms.length === 0){
+          result.innerHTML += "Not Found"
+      }
+      else{
+          for(let i = 0; i< definitions.antonyms.length; i++){
+              result.innerHTML += `<li>${definitions.antonyms[i]}</li>`
+          }
+      }
+    result.innerHTML += `<div><a href = "${data[0].sourceUrls}" target = "_blank">Read more</a></div>`
+  }catch(error){
+    result.innerHTML = `<p>Can't fetch word</p>`
+  } 
 };
